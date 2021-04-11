9bad984eac32d9e4a873c0bf8b2ed84bc832e15a
Gabriela <gabrielafijalkowska@gmail.com>
2021-04-11 17:49:47 +0200

    Add styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import  { unregister } from './core';
+import { GlobalStyles, theme } from './styles';
 
-ReactDOM.render(<div>Sudoku</div>,
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div>Sudoku</div>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
